a0a74d154a0a00cd269abe6804c74015b4f05c40
Guilherme Lemos <coolergate@gmail.com>
2023-12-24 02:10:40 -0300

    Create server main service

---
+++ src/Server/Services/MainService.lua
@@ -0,0 +1,25 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local StarterGui = game:GetService("StarterGui")
+
+local MainService = {Client = {}}
+
+
+function MainService:Start()
+	
+end
+
+
+function MainService:Init()
+	-- -------------------- Remove interfaces from startergui ------------------- --
+	for _, inst in StarterGui:GetChildren() do
+		if not inst:IsA("ScreenGui") then
+			warn("Invalid instance in StarterGui (not a ScreenGui):", inst)
+			continue
+		end
+
+		inst.Parent = self.Shared.EnvironmentFoldersModule:GetReplicatedFolder("interface", ReplicatedStorage)
+	end
+end
+
+
+return MainService
